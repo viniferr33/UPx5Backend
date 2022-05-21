86351f57dbb65181f35109949b68a1776c586e4e
Vinicius Ferreira <vini.ferr33@gmail.com>
2022-05-21 20:26:44 -0300

    added new ws logic

--- src/ws.js
+++ src/ws.js
@@ -1,25 +1,36 @@
 const CronJob = require('cron').CronJob;
+const fs = require('./db/db');
 
-// wss.on('connection', function connection(ws, req) {
-//     ws.id = req.url.split('=')[1];
-//     console.log(`New device connected! - ${ws.id}`);
+function checkFrequency(freq) {
+    const today = new Date();
+    const now = `${today.getHours()}:${today.getMinutes()}`;
     
-//     const ping = new CronJob('*/15 * * * * *', async () => {
-//         const data = await FS.getSchedule(ws.id);
-//         data.forEach(e => {
-//             if(compareDateCron) ws.send(e.operation);
-//             console.log(e);
-//         })
-        
-//     });
-//     ping.start();
-  
-//     ws.on('message', async function incoming(message) {
-//         await FS.createLog(ws.id, message);
-//         console.log(`New log! - ${ws.id} - ${message}`);
-//     });
-//   });
+    const weekdays = JSON.parse(JSON.stringify(freq));
+    delete weekdays.time;
 
-module.exports = (ws, req) => {
+    return freq[Object.keys(weekdays)[today.getDay()]] && now === freq.time;
+}
 
+module.exports = async (ws, req) => {
+    ws.id = req.url.split('=')[1];
+    await fs.createDoc('ws', {
+        url: req.url,
+        datetime: new Date(),
+        message: "Connected"
+    });
+
+    const ping = new CronJob('* */1 * * * *', async () => {
+        const data = await fs.getCollection(`users/${ws.id}/feeding`);
+        data.forEach(e => {
+            if(checkFrequency(e.frequency)) ws.send(JSON.stringify({
+                id: e.id,
+                operation: `${e.operation}:${e.mealSize}`
+            }));
+        });
+    });
+    ping.start();
+
+    ws.on('message', async (message) => {
+        await fs.createDoc(`users/${ws.id}/device`, JSON.parse(message));
+    });
 };
