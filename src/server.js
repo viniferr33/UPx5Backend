b3d8d78d059595c3323e60f41cb6f062f2c352ec
Vinicius Ferreira <vini.ferr33@gmail.com>
2022-05-25 12:03:17 -0300

    added cors middleware

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: "*"
+}));
 
 const routes = require('./routes');
 app.use(routes);
